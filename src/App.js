7a9d416732d6cb2735add6bccd8aa62420626267
metrosso <necatimertmetin@gmail>
2024-07-11 00:07:52 +0300

    last fixes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/pages/menu';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import GamePage from './components/pages/gamePage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={<Menu />}
-        />
-        <Route
-          exact
-          path="/zz"
-          element={<GamePage />}
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route exact path="/" element={<Menu />} />
+      <Route exact path="/zz" element={<GamePage />} />
+    </Routes>
   );
 }
 
